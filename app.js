9a70d78467b132c5ce48154d211db23a0f9f9bc4
David Marquant <david.marquant.93@gmail.com>
2020-09-25 14:19:40 +0200

    Add style, show details on click

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// TODO: Fix page reload when already scrolled
+//
 let RowMinHeight = 18
 let NumRowsShownOnScroll = 200
 let PixelDistToShowMore = 500
@@ -5,6 +7,11 @@ let PixelDistToShowMore = 500
 var numShown
 var intrinsicListDiv
 var filteredIntrinsics = intrinsicsData
+
+function toggleDetails(event) {
+  var detailsId = "details-" + event.target.id
+  document.getElementById(detailsId).classList.toggle("hidden")
+}
 
 function searched(event) {
   var searchTerm = event.target.value.toLowerCase()
@@ -52,11 +59,27 @@ function initialRender() {
   // Create the html for each intrinsic
   intrinsicsData.forEach(intrinsic => {
     html = `
-      <div class="intrinsic">
-        <span>${intrinsic.return_type}</span>
-        <span>${intrinsic.name}</span>
-        <span>(${intrinsic.arg_list})</span>
-        <span>${intrinsic.type}</span>
+      <input id="${intrinsic.name}" class="hidden" type="checkbox" oninput="toggleDetails(event)">
+      <label class="intrinsic" for="${intrinsic.name}">
+        <span class="code">${intrinsic.return_type}</span>
+        <span class="code intrinsic-name">${intrinsic.name}</span>
+        <span class="code">(${intrinsic.arg_list})</span>
+        <span class="code intrinsic-type">${intrinsic.type}</span>
+      </label>
+      <div id="details-${intrinsic.name}" class="hidden details">
+        <h2>Description</h2>
+        <div class="code intrinsic-description">
+          ${intrinsic.description}
+        </div>
+        <h2>Instruction</h2>
+        <span class="code">
+	  <a href="${intrinsic.instruction_ref_link}">${intrinsic.instruction_mnemonic}</a>
+          ${intrinsic.instruction_operands}
+        </span>
+        <h2>Operation</h2>
+        <pre class="code intrinsic-description">
+          ${intrinsic.operation}
+        </pre>
       </div>
     `
     intrinsic["html"] = html
